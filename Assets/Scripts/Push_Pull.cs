761746b7ce9e21561c3a6c89a33a3f1669a7eb0c
Jdworsak <jddworsak@quinnipiac.edu>
2024-09-04 21:53:07 -0400

    Initial commit

---
+++ Assets/Scripts/Push_Pull.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push_Pull : MonoBehaviour
+{
+    public float force = 10f;
+    private Rigidbody2D rb2d;
+    private Camera cam;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Vector2 direction = (mousePos - (Vector2)hit.collider.transform.position).normalized;
+                hit.rigidbody.AddForce(direction * force);
+            }
+        }
+    }
+}
